32476e10ad6fe3804cb28e20a5aef40b5d31d4dd
DHIRAJ GURUNG <gdhiraj030@gmail.com>
2025-07-24 23:51:36 +0300

    final edit footer commit

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,7 +3,7 @@ import { logo } from "../assets/icons/icons";
 
 function Footer() {
   return (
-    <div className="flex py-28 items-center  cursor-pointer bg-gray-950 text-white">
+    <div className="flex py-32 items-center  cursor-pointer bg-gray-950 text-white">
       <div>
         <img src={logo} alt="" />
       </div>
